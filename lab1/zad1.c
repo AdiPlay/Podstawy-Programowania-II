54c11f3bf84831cca236394df0670db93c1febea
AdiPlay <kokoszka.adrian2020@gmail.com>
2023-03-08 12:59:43 +0100

    #

--- lab1/zad1.c
+++ lab1/zad1.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
-    int x[3][3]={{1,2,3},{4,5,6},{7,8,9}};
-    int i,j,n=3,m=3;
-    for(i = 0;i<n;i++){
-        for(j=0;j<m;j++){
-            printf("%d",x[i][j]);
-            printf("\n");
+    int x[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8 ,9}};
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j =0; j < 3; j++)
+        {
+            printf("%d ", x[i][j]);
         }
-        
+        printf("\n");
     }
-    
+
     return 0;
 }
