8e64a6b71fea5e04d304b81b0373c8d49179f515
AdiPlay <kokoszka.adrian2020@gmail.com>
2023-03-08 13:45:13 +0100

    #

--- lab1/zad3.c
+++ lab1/zad3.c
@@ -3,23 +3,31 @@
 int main(int argc, char const *argv[])
 {
 	int x[3][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-	int n = 3, i, j, k, nrW, nrK, sumaW = 0, sumaK = 0;
-	do{
+	int i, j, k, nrW, nrK, sumaW = 0, sumaK = 0;
+	do
+	{
 		printf("Podaj numer wiersza (0-2): ");
 		k = scanf("%d", &nrW);
 		if (k == 0 || nrW > 2)
+		{
 			printf("Blad formatu lub liczba poza zakresem\n");
+			return 1;
+		}
 
 		fflush(stdin);
 
-	}while(k == 0);
-	do{
+	} while (k == 0);
+	do
+	{
 		printf("Podaj numer kolumny (0-2): ");
 		k = scanf("%d", &nrK);
 		if (k == 0 || nrK > 2)
-			printf("Blad formatu \n");
+		{
+			printf("Blad formatu lub liczba poza zakresem\n");
+			return 1;
+		}
 		fflush(stdin);
-	}while(k == 0);
+	} while (k == 0);
 
 	for (j = 0; j < 3; j++)
 		sumaW = sumaW + x[nrW][j];
--- lab1/zad4.c
+++ lab1/zad4.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int n, m, i, j;
     printf("Podaj wymiary macierzy: \n");
     scanf("%d %d", &n, &m);
 
     int macierz[n][m];
-	
-	
-    
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
             if (i == j)
                 macierz[i][j] = 1;
             else
@@ -18,9 +19,10 @@ int main() {
         }
     }
 
-    
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < m; j++) {
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
             printf("%d ", macierz[i][j]);
         }
         printf("\n");
