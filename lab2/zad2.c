642d045020ee76cc1b64673e21165f341483f70c
AdiPlay <19727@student.ans-elblag.pl>
2023-03-08 10:11:54 +0100

    #

--- lab2/zad2.c
+++ lab2/zad2.c
@@ -6,16 +6,14 @@ double newton();
 int main(int argc, char const *argv[])
 {
     printf("Silnia: %.2lf\n", silnia(6));
-    printf("Symbol Newtona: %.2lf\n", newton(5,3));
+    printf("Symbol Newtona: %.2lf\n", newton(3,2));
     return 0;
 }
 
 double silnia(int n)
 {
-    // Warunek końcowy
     if (n == 0 || n == 1)
         return 1;
-    // Wywołanie rekurencyjne
     else
         return n * silnia(n - 1);
 }
