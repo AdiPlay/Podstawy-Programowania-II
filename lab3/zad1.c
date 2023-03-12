8b1d4d1dda84966a99dbc3ca28a8fb67c9ef7e2b
AdiPlay <kokoszka.adrian2020@gmail.com>
2023-03-12 15:26:02 +0100

    #

---
+++ lab3/zad1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    /*a) zdefiniować zmienne x typu int oraz y typu double i zmienne wskaźnikowe px typu
+     wskaźnik do int oraz py typu wskaźnik do double*/
+    int x;
+    double y;
+    int *px;
+    double *py;
+    // b) przypisać zmiennym wskaźnikowym px i py wskaźniki do x i y
+    px = &x;
+    py = &y;
+    // wczytać z klawiatury zmienne x i y przy użyciu wskaźników, następnie je przy użyciu wskaźników
+    printf("Podaj dwie liczby: \n");
+    scanf("%d", px);
+    scanf("%lf", py);
+    printf("Wypisanie zmiennych: \n");
+    printf("%d\n", *px);
+    printf("%lf\n", *py);
+
+    double tab[5];
+    double *pa;
+    pa = tab;
+    printf("Wpisz liczby do tablicy: \n");
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%lf", pa + i);
+    }
+    printf("Drukowanie tablicy: \n");
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%lf\n", *(pa + i));
+    }
+
+    return 0;
+}
