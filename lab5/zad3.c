a847b07e0e96b8dde0c2ee5bd1f035adfaa1ede4
AdiPlay <84020944+AdiPlay@users.noreply.github.com>
2023-03-27 19:51:22 +0200

    Add files via upload

---
+++ lab5/zad3.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void zamienKK(int n, int m, double x[][m], int nrKol1, int nrKol2);
+void zamienWW(int n, int m, double x[][m], int nrWiersza1, int nrWiersza2);
+void transpose(int n, double x[][n]);
+double sarrus(double x[][3]);
+
+
+int main(int argc, char const *argv[])
+{
+    double x[3][3];
+    srand(time(NULL));
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            x[i][j] = rand() % 10;
+        }
+    }
+    printf("Macierz:\n");
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%.2lf\t", x[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Macierz transponowana:\n");
+    transpose(3, x);
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%.2lf\t", x[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Wyznacznik macierzy: %.2lf\n", sarrus(x));
+    
+
+
+    return 0;
+}
+
+void zamienKK(int n, int m, double x[][m], int nrKol1, int nrKol2)
+{
+    double temp;
+    for (int i = 0; i < n; i++)
+    {
+        temp = x[i][nrKol1];
+        x[i][nrKol1] = x[i][nrKol2];
+        x[i][nrKol2] = temp;
+    }
+}
+
+void zamienWW(int n, int m, double x[][m], int nrWiersza1, int nrWiersza2)
+{
+    double temp;
+    for (int j = 0; j < m; j++)
+    {
+        temp = x[nrWiersza1][j];
+        x[nrWiersza1][j] = x[nrWiersza2][j];
+        x[nrWiersza2][j] = temp;
+    }
+}
+
+void transpose(int n, double x[][n])
+{
+    double temp;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            temp = x[i][j];
+            x[i][j] = x[j][i];
+            x[j][i] = temp;
+        }
+    }
+}
+
+double sarrus(double x[][3])
+{
+    double wynik = 0.0;
+    wynik += x[0][0] * x[1][1] * x[2][2];
+    wynik += x[1][0] * x[2][1] * x[0][2];
+    wynik += x[0][1] * x[1][2] * x[2][0];
+    wynik -= x[2][0] * x[1][1] * x[0][2];
+    wynik -= x[1][0] * x[0][1] * x[2][2];
+    wynik -= x[2][1] * x[1][2] * x[0][0];
+    return wynik;
+}
---
+++ lab5/zad4.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+void minmax2D(int n, double x[n][n], double wyniki[]);
+
+int main()
+{
+    double x[4][4] = {{1.0, 2.0, 3.0, 4.0},
+                      {5.0, 6.0, 7.0, 8.0},
+                      {9.0, 10.0, 11.0, 12.0},
+                      {13.0, 14.0, 15.0, 16.0}};
+
+    double wyniki[17];
+    minmax2D(4, x, wyniki);
+
+    printf("Tablica jednowymiarowa:\n");
+    for (int i = 0; i < 16; i++)
+    {
+        printf("%.1lf ", wyniki[i]);
+        if ((i + 1) % 4 == 0)
+        {
+            printf("\n");
+        }
+    }
+    printf("Wartosc minimalna: %.1lf\n", wyniki[16]);
+    printf("Wartosc maksymalna: %.1lf\n", wyniki[17]);
+
+    return 0;
+}
+
+void minmax2D(int n, double x[n][n], double wyniki[])
+{
+    double min = x[0][0];
+    double max = x[0][0];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (x[i][j] < min)
+            {
+                min = x[i][j];
+            }
+            if (x[i][j] > max)
+            {
+                max = x[i][j];
+            }
+            wyniki[i * n + j] = x[i][j];
+        }
+    }
+
+    wyniki[n * n] = min;
+    wyniki[n * n + 1] = max;
+}
