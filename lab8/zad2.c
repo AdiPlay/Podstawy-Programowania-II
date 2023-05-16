44c2b08e2c6f4244b64274ac9331650a9c10ea3c
AdiPlay <19727@student.ans-elblag.pl>
2023-05-16 10:42:12 +0200

    #

--- lab8/zad2.c
+++ lab8/zad2.c
@@ -5,8 +5,7 @@ int main()
     int i;
     for (i = 32; i < 256; i++)
     {
-        printf("\n \n Znak =%c kod dec=%3d hex =%x \
-        oct=%o", i, i, i, i);
+        printf("\nZnak =%c kod dec=%3d hex =%x oct=%o", i, i, i, i);
         getchar();
     }
     return 0;
